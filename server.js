5d32df6f6de3c62a24f3ab6548fc646ea5e22d62
MSmith <marie.ellen@gmail.com>
2022-12-30 16:50:25 -0500

    additional updates

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ MongoClient.connect(dbConnectionString)
         catch{(error => console.error(error))}
     })
 
-    app.get('/api/:mood', (req, res) => {
+    app.get('/:mood', (req, res) => {
         const userMood = req.params.mood.toLowerCase();
 
         collection.find({name: userMood}.toArray())
